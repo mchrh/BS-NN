6616d7df50ac2465c3030cab148db799aaacf617
mchrh <othmachrouh@gmail.com>
2021-04-26 17:47:50 +0200

    getting data from tradier api

--- data/crawler.py
+++ data/crawler.py
@@ -1,9 +1,17 @@
 import requests
+import urllib
 import pandas as pd
 import json
+import simplejson
+from pprint import pprint
 
 response = requests.get('https://sandbox.tradier.com/v1/markets/options/chains',
-    params={'symbol': 'VXX', 'expiration': '2019-05-17', 'greeks': 'true'},
+    params={'symbol': 'APPL', 'expiration': '2019-03-30', 'greeks': 'false'},
     headers={'Authorization': 'Bearer <DOoic2w9mNAX0GUA9hL4vuMRqHzd>', 'Accept': 'application/json'}
 )
-print(response)
+
+if 'json' in response.headers.get('Content-Type'):
+    js = response.json()
+else:
+    print('Response content is not in JSON format.')
+    js = 'spam'
