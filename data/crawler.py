18be9d15af2bb12199d4453616b0f7955963cf8d
mchrh <>
2021-04-26 15:52:35 +0000

    moved project to replit

--- data/crawler.py
+++ data/crawler.py
@@ -3,15 +3,15 @@ import urllib
 import pandas as pd
 import json
 import simplejson
-from pprint import pprint
 
 response = requests.get('https://sandbox.tradier.com/v1/markets/options/chains',
     params={'symbol': 'APPL', 'expiration': '2019-03-30', 'greeks': 'false'},
     headers={'Authorization': 'Bearer <DOoic2w9mNAX0GUA9hL4vuMRqHzd>', 'Accept': 'application/json'}
-)
+).json()
 
-if 'json' in response.headers.get('Content-Type'):
+"""if 'json' in response.headers.get('Content-Type'):
     js = response.json()
 else:
     print('Response content is not in JSON format.')
     js = 'spam'
+"""
