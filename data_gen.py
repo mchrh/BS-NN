0faa840493659039bb6823cd132315976a17df66
mchrh <othmachrouh@gmail.com>
2021-04-28 17:55:14 +0200

    removed old files and added b-s prices with dividends

--- data_gen.py
+++ data_gen.py
@@ -19,6 +19,19 @@ def bs(S,K,T,r,sigma,option):
   return result
 
 
+def bs_div(S,K,T,r,q,sigma,option):
+    d1=(np.log(S/K)+(r-q+0.5*sigma**2)*T)/(sigma*np.sqrt(T))
+    d2=(np.log(S/K)+(r-q-0.5*sigma**2)*T)/(sigma*np.sqrt(T))
+    if option=='call':
+      c=(S*np.exp(-q*T)*si.norm.cdf(d1,0.0,1.0)-K*np.exp(-r*T)*si.norm.cdf(d2,0.0,1.0))
+      result=c
+    elif option=='put':
+      p=(K*np.exp(-r*T)*si.norm.cdf(-d2,0.0,1.0)-S*np.exp(-q*T)*si.norm.cdf(-d1,0.0,1.0))
+      result=p
+
+    return result
+
+
 
 def data_gen_to_csv(N):
     # generating random values for volatility, strike, maturity
@@ -30,10 +43,13 @@ def data_gen_to_csv(N):
     T_upper=1.
     K_lower=0.6
     K_upper=1.2
+    q_lower=0.01
+    q_upper=0.08
 
     sigma=np.random.uniform(sigma_lower,sigma_upper, N)
     maturity=np.random.uniform(T_lower,T_upper,N)
     strike=np.random.uniform(T_lower, T_upper, N)
+    dividend=np.random.uniform(q_lower, q_upper, N)
 
     # computing call prices
     call=[]
@@ -47,4 +63,7 @@ def data_gen_to_csv(N):
     df['K']=strike
     df['Call']=call
 
-    df.to_csv('data/x.csv') # exporting the data as a csv 
+    df.to_csv('data/x.csv') # exporting the data as a csv
+
+
+data_gen_to_csv(10)
