f785e1875334f8c2d07c4bb93f47cf783eef31a2
mchrh <othmachrouh@gmail.com>
2021-04-28 13:44:22 +0200

    scrapping from cboe instead of yfinance

--- scrapping.py
+++ scrapping.py
@@ -1,23 +1,81 @@
-from bs4 import BeautifulSoupimport
-import requests
+# -*- coding: utf-8 -*-
+
+from datetime import date, datetime
+import os
+
+import pandas as pd
+import scrapy
+from scrapy.exceptions import DropItem
+from scrapy.loader import ItemLoader
+from scrapy.http import Request
+
+from scraper import utils
+from scraper.items import DataItem
 
 
-def main():  print(“Hello World!”)if __name__ == “__main__”:  main()
-data_url = “https://finance.yahoo.com/quote/SPY/options"data_html = requests.get(data_url).contentprint(data_html)
+class CBOESpider(scrapy.Spider):
+    name = 'cboe'
+    allowed_domains = ['cboe.com']
+    spider_path = utils.create_spider_path(name)
+    api_endpoint = 'https://cdn.cboe.com/api/global/delayed_quotes/options/'
 
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'scraper.pipelines.CompressAndSavePipeline': 200
+        },
+        'SPIDER_DATA_PATH':
+        spider_path,
+        'FEED_URI':
+        os.path.join(
+            spider_path, 'cboe_feed',
+            '{}_feed_{}.csv'.format(name,
+                                    datetime.now().strftime('%Y%m%d%H%M%S')))
+    }
 
-content = BeautifulSoup(data_html, “html.parser”)
-options_tables = content.find_all(“table”)
+    def __init__(self, *args, **kwargs):
+        super(CBOESpider, self).__init__(*args, **kwargs)
 
-options_tables = [] tables = content.find_all(“table”) for i in range(0, len(content.find_all(“table”))):   options_tables.append(tables[i])
-print(options_tables)
+        if 'SYMBOLS_FILE_PATH' in os.environ:
+            symbols_file = os.environ['SYMBOLS_FILE_PATH']
+            with open(symbols_file, 'r') as f:
+                self.symbols = [symbol.rstrip('\n').upper() for symbol in f]
+        else:
+            self.symbols = CBOESpider._get_all_listed_symbols()
 
-expiration = datetime.datetime.fromtimestamp(int(datestamp)).strftime(“%Y-%m-%d”)
-calls = options_tables[0].find_all(“tr”)[1:] # first row is header
-itm_calls = []otm_calls = []
+    def start_requests(self):
+        for symbol in self.symbols:
+            loader = ItemLoader(item=DataItem())
+            loader.add_value('symbol', symbol)
+            loader.add_value('symbol_path', symbol + '_daily')
+            loader.add_value('start_date', datetime.now().isoformat())
+            loader.add_value(
+                'filename',
+                symbol + '_' + date.today().strftime('%Y%m%d') + '.json')
 
-for call_option in calls:    if “in-the-money” in str(call_option):  itm_calls.append(call_option)  else:    otm_calls.append(call_option)
-itm_call = itm_calls[-1]otm_call = otm_calls[0]
-print(str(itm_call) + “ \n\n “ + str(otm_call))
+            request_url = "{endpoint}{symbol}.json".format(
+                endpoint=CBOESpider.api_endpoint, symbol=symbol)
 
-itm_call_data = [] for td in BeautifulSoup(str(itm_call), “html.parser”).find_all(“td”):   itm_call_data.append(td.text)
+            yield Request(request_url,
+                          callback=self.fetch_data,
+                          meta={'loader': loader})
+
+    def fetch_data(self, response):
+        loader = response.meta['loader']
+
+        content_type = response.headers.get('Content-Type')
+        if not content_type.startswith(b'application/json'):
+            symbol, = loader.get_collected_values('symbol')
+            raise DropItem('Error downloading data for {}'.format(symbol))
+
+        loader.add_value('end_date', datetime.now().isoformat())
+        loader.add_value('data', response.body)
+
+        return loader.load_item()
+
+    def _get_all_listed_symbols():
+        """Returns array of all listed symbols.
+        https://markets.cboe.com/us/options/symboldir/equity_index_options/
+        """
+        url = 'https://markets.cboe.com/us/options/symboldir/equity_index_options/?download=csv'
+        symbols_df = pd.read_csv(url)
+        return symbols_df.iloc[:, 1].array
